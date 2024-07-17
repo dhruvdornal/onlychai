c9ec1b9a43305780e4873905a0ec2600832cfca8
Dhruv Dornal <76276117+dhruvdornal@users.noreply.github.com>
2024-07-17 23:56:32 +0530

    wow
    no

---
+++ script.js
@@ -0,0 +1,20 @@
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const upi = document.getElementById('upi').value;
+    const encodedName = encodeURIComponent(name);
+    const encodedUPI = encodeURIComponent(upi);
+    const thisText = `support.html?name=${encodedName}&upi=${encodedUPI}`;
+    const copyButton = document.getElementById('copyButton');
+
+    copyButton.addEventListener('click', () => {
+      navigator.clipboard.writeText(thisText)
+        .then(() => {
+          alert("Text copied to clipboard")
+        })
+        .catch(err => {
+          alert("Failed to copy")
+        });
+    });
+    // window.location.href = `support.html?name=${encodedName}&upi=${encodedUPI}`; // Navigate with parameters
+}
