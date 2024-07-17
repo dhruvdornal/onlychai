d567b344d863fa9d0dc02b1e81c1c392b9720ad1
Dhruv Dornal <76276117+dhruvdornal@users.noreply.github.com>
2024-07-18 00:21:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function handleFormSubmit(event) {
     const upi = document.getElementById('upi').value;
     const encodedName = encodeURIComponent(name);
     const encodedUPI = encodeURIComponent(upi);
-    // const thisText = `support.html?name=${encodedName}&upi=${encodedUPI}`;
-    // const copyButton = document.getElementById('copyButton');
+    const thisText = `https://dhruvdornal.github.io/onlychai/support.html?name=${encodedName}&upi=${encodedUPI}`;
+    const copyButton = document.getElementById('copyButton');
 
-    // copyButton.addEventListener('click', () => {
-    //   navigator.clipboard.writeText(thisText)
-    //     .then(() => {
-    //       alert("Text copied to clipboard")
-    //     })
-    //     .catch(err => {
-    //       alert("Failed to copy")
-    //     });
-    // });
+    copyButton.addEventListener('click', () => {
+      navigator.clipboard.writeText(thisText)
+        .then(() => {
+          alert("Text copied to clipboard")
+        })
+        .catch(err => {
+          alert("Failed to copy")
+        });
+    });
     window.location.href = `https://dhruvdornal.github.io/onlychai/support.html?name=${encodedName}&upi=${encodedUPI}`; // Navigate with parameters
 }
